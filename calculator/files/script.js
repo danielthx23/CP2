2d6679b5210bc9da5ad9cf15c21a7d8d40036e0d
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-24 22:45:49 -0300

    Segundo CP de Front end

---
+++ calculator/files/script.js
@@ -0,0 +1,47 @@
+
+let operacao = ""
+let calculo = ""
+let resultado
+let display
+
+function digit(value) {
+    if (value === "1" || value === "3" || value === "2" || value === "4" || value === "5" || 
+    value === "6" || value === "7" || value === "8" || value === "9" || value === "0" || value === "+" 
+    || value === "-" || value === "/" || value === "*") {
+        document.getElementById("result").value += value
+    } else if (value === "x") {
+        document.getElementById("result").value += "*"
+
+    }
+}
+
+function equals() {
+        let x = document.getElementById("result").value
+        console.log(x);
+        resolve();
+    }
+
+function addFloat(){
+    document.getElementById("result").value += "."
+}
+
+function resolve() {
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+
+function clearAll() {
+    document.getElementById("result").value = ""
+}
+
+document.addEventListener('keydown', function (event) {
+    const key = event.key
+    console.log(key);
+    if(key === "Enter") {
+        equals()
+    } else if (key === "Backspace") {
+        clearAll()
+    } else  
+    digit(key)
+})
